92620cb65ae58bc899f68bc73ee7a63382637473
Adarsh Krishnan <adarshk7@gmail.com>
2021-02-14 19:19:35 +0200

    Add win screen

---
+++ Area2D.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	for body in get_overlapping_bodies():
+		if 'electron' in body.get_groups():
+			get_tree().change_scene("res://WinScreen.tscn")
